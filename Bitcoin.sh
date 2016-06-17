5da7ab01673b9e273db1ceacb1f2c5d407dcf3c2
Olliecad1 <officialgamermorris@gmail.com>
2016-06-17 19:33:16 +0100

    Update Bitcoin.sh

--- Bitcoin.sh
+++ Bitcoin.sh
@@ -4,8 +4,8 @@ sudo apt-get update
 sudo apt-get -y --force-yes install bitcoin-qt </dev/null
 
 cd ~/
-mkdir .test
-cd .test
+mkdir .bitcoin
+cd .bitcoin
 touch bitcoin.conf
 printf 'listen=1\n   server=1\n   dbcache=50\n   daemon=1\n   testnet=0\n   rpcuser=RPC_USER_NAME\n   rpcpassword=RPC_PASS_WORD' >> bitcoin.conf
 bitcoind &
