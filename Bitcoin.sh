ece0ec9ce1fcb6e8cddf38d6ba650116e05d5daa
Olliecad1 <officialgamermorris@gmail.com>
2016-06-17 19:26:39 +0100

    Bitcoin_Script

---
+++ Bitcoin.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+sudo --assume-yes add-apt-repository ppa:bitcoin/bitcoin
+sudo apt-get update
+sudo apt-get -y --force-yes install bitcoin-qt </dev/null
+
+cd ~/
+mkdir .test
+cd .test
+touch bitcoin.conf
+printf 'listen=1\n   server=1\n   dbcache=50\n   daemon=1\n   testnet=0\n   rpcuser=RPC_USER_NAME\n   rpcpassword=RPC_PASS_WORD' >> bitcoin.conf
+bitcoind &
+
+
+
+
---
+++ README.md
@@ -0,0 +1,4 @@
+Bash script for setting up Bitcoin on ubuntu 
+Step 1, Downloading the bash script git clone https://github.com/olliecad1/ubuntu_bitcoin_setup.git
+Step 2, Executing the Bash script sudo chmod +x ./Bitcoin.sh
+Step 3, Running the script ./Bitcoin.sh
