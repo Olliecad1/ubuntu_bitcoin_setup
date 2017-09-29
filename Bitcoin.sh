59bc0c5ecefe8d4094de9d25add2bfe6e0003a01
Oliver Morris <officialgamermorris@gmail.com>
2017-09-29 17:23:14 +0100

    Update Bitcoin.sh

--- Bitcoin.sh
+++ Bitcoin.sh
@@ -8,7 +8,7 @@ mkdir .bitcoin
 cd .bitcoin
 touch bitcoin.conf
 printf 'listen=1\n   server=1\n   dbcache=50\n   daemon=1\n   testnet=0\n   rpcuser=RPC_USER_NAME\n   rpcpassword=RPC_PASS_WORD' >> bitcoin.conf
-bitcoind &
+bitcoind --daemon
 
 
 
